204acd099c8a161a36cd7b1da4c9aca37322f696
Abhinav Nandekar <abhinavnandekar@gmail.com>
2024-06-27 20:23:57 +0530

    partners

--- src/app/components/pages/customers/customers.component.ts
+++ src/app/components/pages/customers/customers.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { OwlOptions } from 'ngx-owl-carousel-o';
 
 @Component({
   selector: 'app-customers',
@@ -6,5 +7,83 @@ import { Component } from '@angular/core';
   styleUrl: './customers.component.scss'
 })
 export class CustomersComponent {
+  partnerSlides: OwlOptions = {
+		loop: true,
+		nav: false,
+		dots: false,
+		autoplayHoverPause: true,
+		autoplay: true,
+		margin: 30,
+		responsive: {
+			0: {
+				items: 2
+			},
+			576: {
+				items: 4
+			},
+			768: {
+				items: 4
+			},
+			992: {
+				items: 6
+			}
+		}
+    }
 
+    teamSlides: OwlOptions = {
+		loop: true,
+		nav: false,
+		dots: true,
+		autoplayHoverPause: true,
+		autoplay: true,
+		margin: 30,
+		responsive: {
+			0: {
+				items: 1
+			},
+			576: {
+				items: 2
+			},
+			768: {
+				items: 2
+			},
+			992: {
+				items: 3
+			},
+			1500: {
+				items: 5
+			}
+		}
+    }
+    worksSlides: OwlOptions = {
+		loop: true,
+		nav: false,
+		dots: false,
+		autoplayHoverPause: true,
+		autoplay: true,
+		margin: 30,
+		responsive: {
+			0: {
+				items: 1
+			},
+			768: {
+				items: 2
+			},
+			992: {
+				items: 3
+			},
+			1500: {
+				items: 4
+			}
+		}
+    }
+    feedbackSlides: OwlOptions = {
+		loop: true,
+		nav: false,
+		dots: true,
+		autoplayHoverPause: true,
+		autoplay: true,
+		margin: 30,
+		items: 1
+    }
 }
--- src/app/components/pages/partners/partners.component.ts
+++ src/app/components/pages/partners/partners.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { OwlOptions } from 'ngx-owl-carousel-o';
 
 @Component({
   selector: 'app-partners',
@@ -6,5 +7,74 @@ import { Component } from '@angular/core';
   styleUrl: './partners.component.scss'
 })
 export class PartnersComponent {
+  partnerSlides: OwlOptions = {
+		loop: true,
+		nav: false,
+		dots: false,
+		autoplayHoverPause: true,
+		autoplay: true,
+		margin: 30,
+		responsive: {
+			0: {
+				items: 2
+			},
+			576: {
+				items: 4
+			},
+			768: {
+				items: 4
+			},
+			992: {
+				items: 6
+			}
+		}
+    }
 
+    teamSlides: OwlOptions = {
+		loop: true,
+		nav: false,
+		dots: true,
+		autoplayHoverPause: true,
+		autoplay: true,
+		margin: 30,
+		responsive: {
+			0: {
+				items: 1
+			},
+			576: {
+				items: 2
+			},
+			768: {
+				items: 2
+			},
+			992: {
+				items: 3
+			},
+			1500: {
+				items: 5
+			}
+		}
+    }
+    worksSlides: OwlOptions = {
+		loop: true,
+		nav: false,
+		dots: false,
+		autoplayHoverPause: true,
+		autoplay: true,
+		margin: 30,
+		responsive: {
+			0: {
+				items: 1
+			},
+			768: {
+				items: 2
+			},
+			992: {
+				items: 3
+			},
+			1500: {
+				items: 4
+			}
+		}
+    }
 }
